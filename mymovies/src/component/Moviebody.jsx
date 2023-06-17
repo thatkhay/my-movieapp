f7f7ff35391828d772078b593e87f8009a67168a
thatkhay <johnkingsleyegeonu@gmail.com>
2023-06-17 12:23:31 +0100

    movie output styling with miu

--- mymovies/src/component/Moviebody.jsx
+++ mymovies/src/component/Moviebody.jsx
@@ -3,17 +3,35 @@ import TextField from '@mui/material/TextField';
 
 function Moviebody({ movies }) {
   return (
-    <div className='movieBody'>
-      <TextField id="outlined-basic" label="Search movies" variant="outlined" />
+    <div className='mainMovieDiv'>
+    <div className='movieBody' sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 2, marginTop: '2rem' }}>
+      <TextField className='input' id="outlined-basic" label="Search movies" variant="outlined" sx={{marginBottom: 4 }} />
       {movies.map((movie) => (
-        <div className='movieDiv' key={movie.imdbID}>
-          <img className='moviePoster' src={movie.Poster} alt={movie.Title} />
-          <div className='titleAndYear'>
-          <h3 className='movieTitle' >{movie.Title}</h3>
-          <p className='movieYear' >{movie.Year}</p>
-          </div>
+        <div className='movies'
+          key={movie.imdbID}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            margin: 2,
+            padding: 2,
+            backgroundColor: 'primary.main',
+            borderRadius: 1,
+            boxShadow: 2,
+          }}
+        >
+          <img className='moviePoster'
+            src={movie.Poster}
+            alt={movie.Title}
+            sx={{ width: '200px', height: 'auto', marginBottom: 2 }}
+          />
+          <h3 sx={{ color: 'secondary.main', fontSize: '1.2rem', fontWeight: 'bold', marginBottom: 1 }}>
+            {movie.Title}
+          </h3>
+          <p sx={{ color: 'secondary.main', fontSize: '1rem' }}>{movie.Year}</p>
         </div>
       ))}
+    </div>
     </div>
   );
 }
